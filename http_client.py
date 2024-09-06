d2db5feb2730e08ae9a420ecb6bce99f30e76f08
Floodnut <gsniper777@gmail.com>
2024-09-07 00:41:31 +0900

    feat: add form post

--- http_client.py
+++ http_client.py
@@ -5,25 +5,33 @@ class HttpClient:
     def __init__(self, base_url: str, key: str):
         self.base_url = base_url
         self.key = key
-        self.headers = {
+        self.json_headers = {
             'Authorization': f'Token {self.key}',
             'Content-Type': 'application/json',
         }
         
-    def get(self, url: str):
-        return requests.get(url = f'{self.base_url}/{url}', headers=self.headers)
-
-    def post(self, url: str, data: Optional[dict] = None, file_path: Optional[str] = None):
-        if file_path is None:
-            return requests.post(url = f'{self.base_url}/{url}', headers=self.headers, json = data)
+        self.form_headers = {
+            'Authorization': f'Token {self.key}',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
         
-        return requests.post(url = f'{self.base_url}/{url}', headers=self.headers, json = data, files = {'file': open(file_path, 'rb')})
+    def get(self, url: str):
+        return requests.get(url = f'{self.base_url}/{url}', headers=self.json_headers)
+
+    def post(self, url: str, data: Optional[dict] = None):
+        return requests.post(url = f'{self.base_url}/{url}', headers=self.json_headers, json = data)
     
+    def form_post(self, url: str, data: Optional[dict] = None, file_path: Optional[str] = None):
+        if file_path:
+            return requests.post(url = f'{self.base_url}/{url}', headers=self.form_headers, files={'file': open(file_path, 'rb')})
+        
+        return requests.post(url = f'{self.base_url}/{url}', headers=self.form_headers, data=data)
+        
     def put(self, url: str, data: dict):   
-        return requests.put(url = f'{self.base_url}/{url}', headers=self.headers, json = data)
+        return requests.put(url = f'{self.base_url}/{url}', headers=self.json_headers, json = data)
     
     def patch(self, url: str, data: dict):
-        return requests.patch(url = f'{self.base_url}/{url}', headers=self.headers, json = data)
+        return requests.patch(url = f'{self.base_url}/{url}', headers=self.json_headers, json = data)
     
     def delete(self, url: str):
-        return requests.delete(url = f'{self.base_url}/{url}', headers=self.headers)
+        return requests.delete(url = f'{self.base_url}/{url}', headers=self.json_headers)
